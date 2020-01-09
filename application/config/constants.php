1638a8490528e005e5245c86a922ad67b0ebe2c3
Atal Prateek Barla <47524374+atal-prateek@users.noreply.github.com>
2020-01-09 16:43:03 +0530

    Online Config

--- application/config/constants.php
+++ application/config/constants.php
@@ -100,8 +100,8 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	defined('DB_NAME')		? null : define('DB_NAME','db_saone_urban');
 }
 else{
-	defined('DB_HOST')      ? null : define('DB_HOST', 'localhost');
-	defined('DB_USER')      ? null : define('DB_USER', 'root');
-	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', '');
-	defined('DB_NAME')      ? null : define('DB_NAME', '');
+	defined('DB_HOST')      ? null : define('DB_HOST', 'db5000265579.hosting-data.io');
+	defined('DB_USER')      ? null : define('DB_USER', 'dbu414385');
+	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', 'Saoneurban@123#');
+	defined('DB_NAME')      ? null : define('DB_NAME', 'dbs259173');
 }
