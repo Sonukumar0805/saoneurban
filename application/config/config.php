51ea76e54325836b8c285241c2b026f455c9cc1e
Sonu Kumar <sonu.kumar@brightcodess.com>
2020-01-09 17:14:58 +0530

    file access

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,7 @@ if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']== 'localhost'){
 	$config['base_url'] = "http://$_SERVER[HTTP_HOST]/saoneurban/";
 }
 elseif(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='softwarenew.in'){
-	$config['base_url'] = "http://$_SERVER[HTTP_HOST]/saoneurban/";
+	$config['base_url'] = "http://$_SERVER[HTTP_HOST]/saoneurban/index.php";
 }
 else{
 	$config['base_url'] = "http://$_SERVER[HTTP_HOST]";
